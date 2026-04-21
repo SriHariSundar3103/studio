8d9e12f764c58f51c315c80c91ca781b81ba6c5e
Sri Hari <sri352006@gmail.com>
2026-04-21 05:23:02 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,10 +2,10 @@
 // values with your project's own Firebase configuration which you can
 // find in the Firebase console.
 export const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id",
+  "projectId": "studio-5884151054-1444a",
+  "appId": "1:738502487696:web:3e77cfcff203206814b215",
+  "apiKey": "AIzaSyBRl8kBfsMna7shE5VE0ANZ2kkLdscU1iQ",
+  "authDomain": "studio-5884151054-1444a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "738502487696"
 };
