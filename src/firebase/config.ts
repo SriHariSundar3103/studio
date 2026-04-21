35809d7929b96597646907585f0cd01cf83de0fb
Sri Hari <sri352006@gmail.com>
2026-04-21 05:27:42 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,3 @@
-// Note: This is a sample configuration. You will need to replace these
-// values with your project's own Firebase configuration which you can
-// find in the Firebase console.
 export const firebaseConfig = {
   "projectId": "studio-5884151054-1444a",
   "appId": "1:738502487696:web:3e77cfcff203206814b215",
